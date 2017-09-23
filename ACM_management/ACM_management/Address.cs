79d1134f17ecb600cfdf62dfec2a3bc8fa3368a1
manju <manjupaul1@gmail.com>
2017-09-23 16:39:13 -0400

    Added Address class

---
+++ ACM_management/ACM_management/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM_management
+{
+
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int AddressId)
+        {
+            this.AddressId = AddressId;
+
+        }
+    
+        public int AddressId { get; set; }
+        public int AddressType { get; set; }
+        public string Streetline1 { get; set; }
+        public string Streetline2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Postalcode { get; set; }
+        public string Country { get; set; }
+    }
+}
+
